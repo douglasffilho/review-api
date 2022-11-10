3bb960aba929fbfb31bc665c837fc05420dd1ef1
Douglas Filho <douglasf.filho@gmail.com>
2022-11-10 15:19:19 -0300

    fix: 🐛 prod script

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const { default: fastify } = require('fastify')
+const process = require('process')
+const app = require('.')
+
+const server = fastify({
+  logger: true,
+  pluginTimeout: 10000
+})
+
+server.register(app)
+
+server.listen({ port: process.env.PORT ?? 8080 }, (err) => {
+  if (err) {
+    server.log.error(err)
+    throw err
+  }
+})
